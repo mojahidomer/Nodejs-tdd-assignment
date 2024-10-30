8b4c3462d01eb4449879357c299736381a623faa
mojahidomer <38102741+mojahidomer@users.noreply.github.com>
2024-10-30 13:15:29 +0530

    test: should return expected o/p for the given test input

--- TDD/add.js
+++ TDD/add.js
@@ -1,7 +1,12 @@
-const add   =(string)=>{
-    if(!string){
+const add = (input) => {
+    if (!input) {
+
         return 0
     }
+    return input
+        .split(',')         
+        .map(Number)        
+        .reduce((sum, num) => sum + num, 0)
 }
 
 module.exports = add

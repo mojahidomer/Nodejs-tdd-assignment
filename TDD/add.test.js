42aa7cae627cb33586e41a2042992f056f8a3c15
mojahidomer <38102741+mojahidomer@users.noreply.github.com>
2024-10-30 13:11:22 +0530

    test: should failed test cases for the expected return

--- TDD/add.test.js
+++ TDD/add.test.js
@@ -1,7 +1,12 @@
 const add  =  require("./add")
 
 describe('String number Calculator', () => {
-    test('returns 0 for an empty string', () => {
+    it('should returns 0 for an empty string', () => {
       expect(add("")).toBe(0);
     });
+
+    it("should returns the sum of comma-separated numbers", () => {
+        expect(add("1,2,3")).toBe(6);
+        expect(add("4,5")).toBe(9);
+      });
 })

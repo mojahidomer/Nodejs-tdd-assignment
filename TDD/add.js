5b83536c812bce07e194fc90c5ca56a329e11327
mojahidomer <38102741+mojahidomer@users.noreply.github.com>
2024-10-31 15:40:08 +0530

    TDD: should passed test case for dynamic delimiter

--- TDD/add.js
+++ TDD/add.js
@@ -3,8 +3,15 @@ const add = (input) => {
 
         return 0
     }
-    const delimiter = /,|\n/
-    return input
+    let delimiter = /,|\n/
+    let numberString = input;
+
+    if (input.startsWith("//")) {
+        const delimiterLineEndIndex = input.indexOf("\n");
+        delimiter = new RegExp(input.substring(2, delimiterLineEndIndex).replace(/[.*+?^${}()|[\]\\]/g, '\\$&')); // Escape special regex chars
+        numberString = input.substring(delimiterLineEndIndex + 1);
+      }
+    return numberString
         .split(delimiter)         
         .map(Number)        
         .reduce((sum, num) => sum + num, 0)

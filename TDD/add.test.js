a25aae5ba1426075b904974d5a5aca28fc2590b6
mojahidomer <38102741+mojahidomer@users.noreply.github.com>
2024-10-31 16:03:22 +0530

    TDD: impl test cases to ignor number greater than 1000

--- TDD/add.test.js
+++ TDD/add.test.js
@@ -1,25 +1,30 @@
-const add  =  require("./add")
+const add = require("./add")
 
 describe('String number Calculator', () => {
     it('should returns 0 for an empty string', () => {
-      expect(add("")).toBe(0);
+        expect(add("")).toBe(0);
     });
 
     it("should returns the sum of comma-separated numbers", () => {
         expect(add("1,2,3")).toBe(6);
         expect(add("4,5")).toBe(9);
-      });
-      it("should returns the sum of comma-separated value along with new lines", () => {
+    });
+    it("should returns the sum of comma-separated value along with new lines", () => {
         const newLineInput = "1\n2,3"
         expect(add(newLineInput)).toBe(6);
-      });
-      it("should returns the sum of comma-separated value when passed different delimiter", () => {
+    });
+    it("should returns the sum of comma-separated value when passed different delimiter", () => {
         const newDelimiter = "//;\n1;2"
         expect(add(newDelimiter)).toBe(3);
-      });
-      it("should returns  error message for negative input ", () => {
-        expect(()=>add("1,-2,3")).toThrow("Negative numbers not allowed: -2");
-        expect(()=>add("1,-2,-3")).toThrow("Negative numbers not allowed: -2, -3");
-      });
+    });
+    it("should returns  error message for negative input ", () => {
+        expect(() => add("1,-2,3")).toThrow("Negative numbers not allowed: -2");
+        expect(() => add("1,-2,-3")).toThrow("Negative numbers not allowed: -2, -3");
+    });
+    it("should ignore number greater than 1000", () => {
+        expect(add("1,1001,3000")).toBe(1);
+        expect(() => add("1,2000,4")).toBe(5);
+    });
+
 })
 
